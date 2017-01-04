1b5ad975e4728d48f3356ede2bfededb2aa9c519
Victor Lap <victorlap@outlook.com>
2017-01-04 19:22:34 +0100

    Create board test stub

---
+++ src/utwente/ss/connect/test/BoardTest.java
@@ -0,0 +1,36 @@
+package utwente.ss.connect.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import utwente.ss.connect.common.model.Bead;
+import utwente.ss.connect.common.model.Board;
+import utwente.ss.connect.common.model.Colour;
+
+public class BoardTest {
+	
+	Board board;
+
+	@Before
+	public void setUp() throws Exception {
+		board = new Board();
+	}
+
+	@Test
+	public void testHasWinner() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDoMove() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFallToPlace() {
+		fail("Not yet implemented");
+	}
+
+}
