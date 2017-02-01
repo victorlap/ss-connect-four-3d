2ebe234108e81d74cec652cf484787d67d33003a
niek khasuntsev <Niek@MacBook-Pro-van-niek.local>
2017-02-01 20:27:24 +0100

    Added hint function for HumanPlayer

--- src/utwente/ss/connect/common/model/players/HumanPlayer.java
+++ src/utwente/ss/connect/common/model/players/HumanPlayer.java
@@ -1,10 +1,13 @@
 package utwente.ss.connect.common.model.players;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import utwente.ss.connect.common.exception.BadMoveException;
 import utwente.ss.connect.common.model.Bead;
 import utwente.ss.connect.common.model.Board;
+import utwente.ss.connect.common.model.strategies.SmartStrategy;
+import utwente.ss.connect.common.model.strategies.Strategy;
 
 /**
  * @author Niek Khasuntsev
@@ -12,6 +15,7 @@ import utwente.ss.connect.common.model.Board;
 public class HumanPlayer extends Player {
 
 	private Scanner in;
+	private SmartStrategy strategy;
 
 	// -- Constructors -----------------------------------------------
 
@@ -22,8 +26,8 @@ public class HumanPlayer extends Player {
 	public HumanPlayer(String name, Bead bead) {
 		super(name, bead);
 	}
-	
-	public HumanPlayer(){
+
+	public HumanPlayer() {
 		super();
 	}
 	// -- Commands ---------------------------------------------------
@@ -33,7 +37,9 @@ public class HumanPlayer extends Player {
 		int[] choice = new int[2];
 		System.out.println(getName() + "(" + getBead() + ")" + ", your turn!");
 		in = new Scanner(System.in);
+
 		System.out.println("Do you want a hint? Y/N");
+
 		boolean valid = false;
 		while (!valid) {
 			try {
@@ -42,6 +48,9 @@ public class HumanPlayer extends Player {
 				if (input.equals("n")) {
 					valid = true;
 				} else if (input.equals("y")) {
+					strategy = new SmartStrategy();
+					int[] possible = strategy.generateMove(getBead());
+					System.out.println("Psst! Look at:  x = " + possible[0] + " and z = " + possible[1] + " ;)");
 					valid = true;
 				} else {
 					throw new BadMoveException();
@@ -75,6 +84,16 @@ public class HumanPlayer extends Player {
 			}
 		}
 		return choice;
+	}
+
+	private int readInt() {
+		int answer = -1;
+		do {
+			answer = in.nextInt();
+			in.nextLine();
+			// in.nextLine(); // Needed because nextInt doesnt skip to next line
+		} while (answer == -1);
+		return answer;
 	}
 
 }
